f39224181b9c30748b61d17e427a01cbe350553b
hywenklis <hywenklis@hotmail.com>
2022-09-03 02:50:05 -0300

    cliente e servidor com socket - caixa eletrônico

--- PI-2/cliente/cliente.py
+++ PI-2/cliente/cliente.py
@@ -1,9 +1,13 @@
+import time
 from socket import AF_INET, SOCK_STREAM, socket
 from getpass import getpass
+from os import system
 
 HOST = '127.0.0.1'
 PORTA = 50000
 AUTORIZADO = "Autenticado com Sucesso! Bem vindo ao nosso sistema :D"
+
+clear = lambda: system('clear')
 
 conexao = socket(AF_INET, SOCK_STREAM)
 conexao.connect((HOST, PORTA))
@@ -20,15 +24,71 @@ def envia_senha():
     conexao.send(senha.encode())
 
 
+def menu():
+    clear()
+    print('=' * 30)
+    print("{:^30}".format("MENU DE OPERAÇÕES"))
+    print('=' * 30)
+    print('Opções:')
+    print('1 - Extrato')
+    print('2 - Depósito')
+    print('3 - Saque')
+    print('4 - Sair')
+    print('=' * 30)
+    print('=' * 30)
+
+
+def condicao(aperte):
+    if aperte == 'X' or aperte == 'x':
+        exit()
+    else:
+        menu()
+
+
 while True:
     envia_login()
     envia_senha()
 
-    resposta = conexao.recv(1024)
-    print("Servidor: ", resposta.decode())
+    resposta = conexao.recv(2048)
+    print("Servidor: ", resposta.decode('UTF-8'))
 
     if resposta.decode() == AUTORIZADO:
-        break
-
-print("Fechando a conexão")
-conexao.close()
+        time.sleep(1.5)
+        menu()
+        while True:
+            opcao = input('Digite a opção desejada: ')
+            if opcao == '1':
+                conexao.send(opcao.encode())
+                msg = conexao.recv(2048).decode('UTF-8')
+                print(msg)
+                print()
+                print("Aperte ENTER para retornar ao MENU ou (X e ENTER) para sair: ")
+                aperte = input()
+                condicao(aperte)
+            elif opcao == "2":
+                conexao.send(opcao.encode())
+                print("Digite o valor do deposito: ")
+                valor = input()
+                conexao.send(valor.encode())
+                msg2 = conexao.recv(2048).decode("UTF-8")
+                print(msg2)
+                print("Aperte ENTER para retornar ao MENU ou (X e ENTER) para sair: ")
+                aperte = input()
+                condicao(aperte)
+            elif opcao == "3":
+                conexao.send(opcao.encode())
+                print("Digite o valor que deseja sacar: ")
+                valor = input()
+                conexao.send(valor.encode())
+                msg3 = conexao.recv(2048).decode("UTF-8")
+                print(msg3)
+                print("Aperte ENTER para retornar ao MENU ou (X e ENTER) para sair: ")
+                aperte = input()
+                condicao(aperte)
+            elif opcao == "4":
+                print("Aperte ENTER para retornar ao MENU ou (X e ENTER) para sair: ")
+                aperte = input()
+                condicao(aperte)
+            else:
+                print("Opção inválida")
+                continue
--- PI-2/servidor/servidor.py
+++ PI-2/servidor/servidor.py
@@ -1,9 +1,19 @@
-from socket import AF_INET, SOCK_STREAM, socket
+from socket import *
+
+
+class Conta:
+    def __init__(self, cliente, senha, agencia, numero_conta, saldo):
+        self.cliente = cliente
+        self.senha = senha
+        self.agencia = agencia
+        self.numero_conta = numero_conta
+        self.saldo = saldo
+
+
+pessoa = Conta('hywenklis', '1234', '0001', '1010-2', 0)
 
 HOST = 'localhost'
 PORTA = 50000
-LOGIN = "hywenklis"
-SENHA = "1234"
 
 sockobj = socket(AF_INET, SOCK_STREAM)
 sockobj.bind((HOST, PORTA))
@@ -16,7 +26,7 @@ def autenticando_cliente():
     login_decodificado = recupera_login_do_cliente()
     senha_decodificado = recupera_senha_do_cliente()
     while FALHA:
-        if login_decodificado != LOGIN or senha_decodificado != SENHA:
+        if login_decodificado != pessoa.cliente or senha_decodificado != pessoa.senha:
             print(f"Cliente: login={login_decodificado} senha={senha_decodificado}")
             nao_autorizado = "Não foi possível se conectar, tente novamente"
             conexao.send(nao_autorizado.encode())
@@ -25,7 +35,12 @@ def autenticando_cliente():
             print(f"Cliente: login={login_decodificado} senha={senha_decodificado}")
             autorizado = "Autenticado com Sucesso! Bem vindo ao nosso sistema :D"
             conexao.send(autorizado.encode())
-        break
+            break
+
+
+def envia_comando(conexao, comando):
+    comando += '\r\n'
+    conexao.send(comando.encode('UTF-8'))
 
 
 def recupera_login_do_cliente():
@@ -40,14 +55,64 @@ def recupera_senha_do_cliente():
     return senha_decodificado
 
 
+def recupera_opcao_do_cliente():
+    opcao = conexao.recv(1024)
+    opcao_decodificada = opcao.decode()
+    return opcao_decodificada
+
+
+def recupera_valor_do_deposito():
+    valor_depositado = conexao.recv(1024)
+    valor_depositado_decodificado = valor_depositado.decode()
+    return float(valor_depositado_decodificado)
+
+
+def recupera_valor_de_saque():
+    valor_para_sacar = conexao.recv(1024)
+    valor_para_sacar_decodificado = valor_para_sacar.decode()
+    return float(valor_para_sacar_decodificado)
+
+
+def extrato(self):
+    envia_comando(conexao, f"seu saldo é: R${self.saldo}".replace(".", ","))
+
+
+def depositar(valor):
+    pessoa.saldo += valor
+    envia_comando(conexao, "Depósito realizado!")
+
+
+def sacar(valor):
+    if valor <= pessoa.saldo:
+        pessoa.saldo -= valor
+        envia_comando(conexao, "Saque realizado!")
+    else:
+        envia_comando(conexao, 'Saldo insuficiente.')
+
+
+def fecha_conexao():
+    exit()
+
+
 def conexao_entre_cliente_e_servidor():
-    global conexao
     while True:
+        global conexao
         conexao, endereco = sockobj.accept()
         print("Conectado em", endereco)
         autenticando_cliente()
-        print("fechando a conexão")
-        break
+        while True:
+            opcao = recupera_opcao_do_cliente()
+            if opcao == "1":
+                extrato(pessoa)
+            elif opcao == "2":
+                valor_deposito = recupera_valor_do_deposito()
+                depositar(valor_deposito)
+            elif opcao == "3":
+                valor_saque = recupera_valor_de_saque()
+                sacar(valor_saque)
+            else:
+                print("Fechando conexão!")
+                fecha_conexao()
 
 
 conexao_entre_cliente_e_servidor()
